9d665fe5635651df97a0f2da4222de563d0d198b
Gabriel P <48415896+GaghiZor@users.noreply.github.com>
2022-03-17 22:16:11 +0200

    Fixed google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -6,43 +6,29 @@ import Head from "next/head";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "../theme";
 import Layout from "../components/Layout";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-import * as gtag from "../lib/gtag";
+
 import Script from "next/script";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
   return (
     <>
       {/* Global Site Tag (gtag.js) - Google Analytics */}
       <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
       />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
+      <Script strategy="lazyOnload">
+        {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+              
               page_path: window.location.pathname,
             });
-          `,
-        }}
-      />
+        `}
+      </Script>
+
       <SessionProvider session={session}>
         <ChakraProvider>
           <AppContextProvider>
